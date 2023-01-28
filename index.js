d0f90e9453b5992f20be3b082d04a611488caa0a
S. M. Samiul Alam <s.m.samiulalam15@gmail.com>
2023-01-28 11:56:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+document.addEventListener('click', myFunction)
+
+function myFunction() {
+
+if(document.getElementById("myfile").id=true){
+  f= document.createElement("INPUT");
+  f.setAttribute("type", "file");
+  document.body.appendChild(f);
+}
+}
+
+/*document.addEventListener('click', myFunction)
+
+let headers=document.getElementsByClassName("myfiles")
+
+headers=Array.from(headers)
+
+var a=headers.forEach(header => {console.log
+(header)  
+});
+
+
+function myFunction() {
+
+  if(document.getElementsByClassName("a")="myfiles"){
+    f= document.createElement("INPUT");
+    f.setAttribute("type", "file");
+    document.body.appendChild(f);
+  }
+}*/
